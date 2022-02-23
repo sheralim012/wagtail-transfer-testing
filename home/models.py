f1cf94c101c0b60623117d6349f5415484eeb111
Sher Ali <sheralim012@gmail.com>
2022-02-23 14:33:37 +0500

    basic setup for testing

---
+++ home/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import PageChooserPanel
+from wagtail.core.blocks import PageChooserBlock
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import (
+    PageChooserPanel,
+    StreamFieldPanel,
+)
+
+
+class HomePage(Page):
+    featured_page = models.ForeignKey(
+        'wagtailcore.Page', null=True, blank=True, on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        PageChooserPanel('featured_page'),
+    ]
+
+
+class PageButtonBlock(blocks.StructBlock):
+    page = blocks.PageChooserBlock()
+    text = blocks.CharBlock(required=False, max_length=255)
+
+
+class Article(Page):
+    body = StreamField([
+        ('page_button', PageButtonBlock()),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+
+class Poll(Page):
+    pass 
